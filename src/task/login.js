1d31fbb4d93d74b1a6bad34122a32758211acfd5
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2024-04-03 12:49:08 -0300

    refactor: :recycle: Refatoração
    Exclusão dos arquivos antigos que foram refatorados

--- src/task/login.js
+++
@@ -1,9 +0,0 @@
-import { checkCookie } from "../session/checkCookie.js";
-import { redirectToLogin } from "../session/redirectToLogin.js";
-
-// Verificar se o cookie "username" existe
-if (!checkCookie("id")) {
-  // Se o cookie "username" não existir, redirecionar para a página de login
-  redirectToLogin();
-}
-
